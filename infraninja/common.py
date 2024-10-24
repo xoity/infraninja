8afdc45e30329782d16559812c9f9a0f5fef97e7
mohamad abukhader <51475926+xoity@users.noreply.github.com>
2024-10-24 10:48:44 +0400

    Update common.py

--- infraninja/common.py
+++ infraninja/common.py
@@ -1,4 +1,6 @@
-from pyinfra.operations import apt, apk
+from pyinfra.operations import apt, apk, files
+from pyinfra import host
+from pyinfra.api import deploy
 
 @deploy('Common System Updates')
 def system_update():
@@ -10,3 +12,21 @@ def system_update():
     elif os == 'Alpine':
         apk.update(name="Update Alpine package lists", sudo=True)
         apk.upgrade(name="Upgrade Alpine packages", sudo=True)
+    
+@deploy("ssh common hardening test")
+def ssh_common_hardening():
+
+    ssh_config = {
+        "PermitRootLogin": "prohibit-password",
+        "PasswordAuthentication": "no",
+        "X11Forwarding": "no",
+    }
+
+    # Apply SSH configuration
+    for option, value in ssh_config.items():
+        files.line(
+            name=f"Configure SSH: {option}",
+            path="/etc/ssh/sshd_config",
+            line=f"{option} {value}",
+            sudo=True,
+        )
