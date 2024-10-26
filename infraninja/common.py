4f59354f2ed834528989b413e34919bdaf334f9d
mohamad abukhader <51475926+xoity@users.noreply.github.com>
2024-10-26 16:52:52 +0400

    Update common.py

--- infraninja/common.py
+++ infraninja/common.py
@@ -1,17 +1,20 @@
 from pyinfra.operations import apt, apk, files
-from pyinfra import host
+from pyinfra import host, config
 from pyinfra.api import deploy
+from pyinfra.facts.server import LinuxName
+
+os = host.get_fact(LinuxName)  
+config.SUDO = True
 
 @deploy('Common System Updates')
 def system_update():
-    os = host.fact.linux_name
 
     if os == 'Ubuntu':
-        apt.update(name="Update Ubuntu package lists", sudo=True)
-        apt.upgrade(name="Upgrade Ubuntu packages", sudo=True)
+        apt.update(name="Update Ubuntu package lists")
+        apt.upgrade(name="Upgrade Ubuntu packages")
     elif os == 'Alpine':
-        apk.update(name="Update Alpine package lists", sudo=True)
-        apk.upgrade(name="Upgrade Alpine packages", sudo=True)
+        apk.update(name="Update Alpine package lists")
+        apk.upgrade(name="Upgrade Alpine packages")
     
 @deploy("ssh common hardening test")
 def ssh_common_hardening():
@@ -27,6 +30,5 @@ def ssh_common_hardening():
         files.line(
             name=f"Configure SSH: {option}",
             path="/etc/ssh/sshd_config",
-            line=f"{option} {value}",
-            sudo=True,
+            line=f"{option} {value}"
         )
