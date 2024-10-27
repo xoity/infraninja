5ac93c47025d670f4b97eadb178da73eaffbef17
mohamad abukhader <51475926+xoity@users.noreply.github.com>
2024-10-27 12:38:19 +0400

    Update test_deploy.py

--- test/test_deploy.py
+++ test/test_deploy.py
@@ -1,11 +1,11 @@
 import pyinfra
 from pyinfra.api import deploy
 from infraninja.security import security_setup
-from infraninja.common import system_update, ssh_common_hardening
+from infraninja.common import system_update, ssh_common_hardening, uae_ia_compliance
 from pyinfra import host 
 
 
-@deploy('Test Security Setup and Docker')
+@deploy('Test Security Setup')
 def test_deploy():
     # Run the security setup
     security_setup()
@@ -13,6 +13,8 @@ def test_deploy():
     system_update()
     # Run SSH hardening
     ssh_common_hardening()
+    # Run UAE IA compliance
+    uae_ia_compliance()
 
 
 
