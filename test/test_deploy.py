50a8e39f4a42caafbf68ab7dfae13cbce8ac4a4f
mohamad abukhader <51475926+xoity@users.noreply.github.com>
2024-11-04 12:54:41 +0400

    Update test_deploy.py

--- test/test_deploy.py
+++ test/test_deploy.py
@@ -1,21 +1,18 @@
 import pyinfra
 from pyinfra.api import deploy
 from infraninja.security import security_setup
-from infraninja.common import system_update, ssh_common_hardening, uae_ia_compliance
+from infraninja.common import system_update, ssh_common_hardening, uae_ia_compliance, disable_useless_services_common
 from pyinfra import host 
 
 
 @deploy('Test Security Setup')
 def test_deploy():
-    # Run the security setup
     security_setup()
-    # Run common system updates
     system_update()
-    # Run SSH hardening
     ssh_common_hardening()
-    # Run UAE IA compliance
     uae_ia_compliance()
-
+    disable_useless_services_common()
+    
 
 
 # Execute the deploy on the VMs
