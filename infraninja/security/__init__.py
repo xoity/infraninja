56ef2e7b649afa66d6b6d8c86acf327ab8a77318
mohamad abukhader <51475926+xoity@users.noreply.github.com>
2024-11-06 10:43:09 +0400

    Update __init__.py

--- infraninja/security/__init__.py
+++ infraninja/security/__init__.py
@@ -1,3 +1,5 @@
 # security/__init__.py
-from .ssh_hardening import ssh_hardening
-from .uae_ia_compliance import uae_ia_compliance
+from security.common.ssh_hardening import ssh_hardening
+from security.common.uae_ia_compliance import uae_ia_compliance
+from security.common.update_packages import system_update
+from security.common.disable_services import disable_useless_services_common
