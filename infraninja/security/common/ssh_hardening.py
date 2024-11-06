a9a7a53c2c612d9613faa9f4b93f295d7718d7ed
mohamad abukhader <51475926+xoity@users.noreply.github.com>
2024-11-06 10:35:51 +0400

    Update ssh_hardening.py

--- infraninja/security/common/ssh_hardening.py
+++ infraninja/security/common/ssh_hardening.py
@@ -1,6 +1,4 @@
-# security/common/ssh_hardening.py
-
-from pyinfra.operations import files
+from pyinfra.operations import files, systemd
 from pyinfra.api import deploy
 
 ssh_config = {
@@ -11,10 +9,28 @@ ssh_config = {
 
 @deploy("SSH Hardening")
 def ssh_hardening():
+    # Track whether any changes were made
+    config_changed = False
+    
     for option, value in ssh_config.items():
-        files.replace(
+        # Apply each SSH configuration setting
+        change = files.replace(
             name=f"Configure SSH: {option}",
             path="/etc/ssh/sshd_config",
             line=f"^{option} .*$",
             replace=f"{option} {value}",
+        )
+        
+        # If any change was detected, set `config_changed` to True
+        if change.changed:
+            config_changed = True
+    
+    # If any changes were made, reload and restart the SSH service
+    if config_changed:
+        systemd.daemon_reload()  # Reload systemd to pick up changes
+        systemd.service(
+            name="Restart SSH",
+            service="ssh",
+            running=True,
+            restarted=True,
         )
