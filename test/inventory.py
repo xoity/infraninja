2bc263fb6a7dc012c49b316fb4063ff1ed49a0af
Mohammad Abukhader <mohammad@kalvad.com>
2024-11-22 16:53:32 +0400

    modified:   test/inventory.py 	modified:   test/test_deploy.py

--- test/inventory.py
+++ test/inventory.py
@@ -1,12 +1,11 @@
 import os
 import requests
 
+
 # defined the hosts variable as a list of tuples
 def fetch_servers(access_key):
-    url = "https://jinn-beta.kalvad.cloud/inventory/getServers/"
-    headers = {
-        "Authentication": access_key
-    }
+    url = os.getenv("INVENTORY_URL")
+    headers = {"Authentication": access_key}
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
@@ -18,7 +17,10 @@ def fetch_servers(access_key):
                     **server.get("attributes", {}),
                     "ssh_user": server.get("ssh_user"),
                     "is_active": server.get("is_active", False),
-                    "install_postgres": server.get("attributes", {}).get("docker", "False") == "True",
+                    "install_postgres": server.get("attributes", {}).get(
+                        "docker", "False"
+                    )
+                    == "True",
                     "group_name": server.get("group", {}).get("name_en"),
                 },
             )
@@ -36,6 +38,7 @@ def fetch_servers(access_key):
         print("An unexpected error occurred:", e)
         print("No hosts will be fetched.")
         return []
+
 
 # Example usage
 access_key = os.getenv("ACCESS_KEY")  # Read the access key from environment variable
--- test/test_deploy.py
+++ test/test_deploy.py
@@ -1,29 +1,14 @@
 import pyinfra
 from pyinfra.api import deploy
 from infraninja.security.common.update_packages import system_update
-from infraninja.security.common.acl import acl_setup
-from inventory import fetch_servers
 
-@deploy('Test Security Setup')
+
+@deploy("Test Security Setup")
 def test_deploy():
+    # define any deploys & functions below!
     system_update()
-    acl_setup()  # Add this line to apply ACL rules
-
-def main():
-    access_key = input("Enter your access key: ")
-    hosts = fetch_servers(access_key)
-    if not hosts:
-        print("No valid hosts found.")
-        return
-
-    # Execute the deploy on the fetched VMs
-    pyinfra.api.deploy(
-        test_deploy(),
-        hosts=hosts
-    )
-
-if __name__ == "__main__":
-    main()
 
 
-
+pyinfra.api.deploy(
+    test_deploy(),
+)
