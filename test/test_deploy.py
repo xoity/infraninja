28d42c7ff4cc06b03a1dcb2997ab2317621ee7e9
mohamad abukhader <51475926+xoity@users.noreply.github.com>
2024-12-19 12:04:54 +0400

    renamed:    infraninja/security/common/arp_poisoning_protection_alpine.py -> infraninja/security/common/arp_poisoning_protection.py 	modified:   test/test_deploy.py

--- test/test_deploy.py
+++ test/test_deploy.py
@@ -1,16 +1,12 @@
 import pyinfra
 from pyinfra.api import deploy
-from infraninja.security.common.kernel_hardening import kernel_hardening as task2
 from infraninja.security.common.password_policy import password_policy as task3
-from infraninja.security.ubuntu.apparmor_config import apparmor_config as task4
 
 @deploy('Test Security Setup')
 def test_deploy():
 
     # define any deploys & functions below! add sudo=True to any operations that require it
-    task2(_sudo=True)
     task3(_sudo=True)
-    task4(_sudo=True)
 
 
 
