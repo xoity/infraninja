d6b3706ee93476b3a2d705d198243ec58c6686b5
mohamad abukhader <51475926+xoity@users.noreply.github.com>
2024-12-20 13:38:25 +0400

    Update T3.6.3.py

--- infraninja/security/common/UAE_IA_COMPLIANCE/T3.6.3.py
+++ infraninja/security/common/UAE_IA_COMPLIANCE/T3.6.3.py
@@ -9,5 +9,4 @@ def T3_6_3():
         name="Log privileged operations and unauthorized access",
         path="/etc/audit/audit.rules",
         line="-w /sbin/ifconfig -p x -k network",
-        _sudo=True,
     )
