9300db213986097dbd1781992c8c79346db8c5b5
mohamad abukhader <51475926+xoity@users.noreply.github.com>
2024-12-21 14:08:06 +0400

    modified:   infraninja/security/common/disable_services.py

--- infraninja/security/common/disable_services.py
+++ infraninja/security/common/disable_services.py
@@ -13,10 +13,18 @@ common_services = ["avahi-daemon", "cups", "bluetooth", "rpcbind", "vsftpd", "te
 def disable_useless_services_common():
     for service in common_services:
         if os == "Ubuntu":
-            systemd.service(service, running=False, enabled=False)
-            print(f"Disabled service: {service} on Ubuntu")
+            service_status = host.run_shell_command(f"systemctl is-active {service}")
+            if service_status.stdout.strip() == "active":
+                systemd.service(service, running=False, enabled=False)
+                print(f"Disabled service: {service} on Ubuntu")
+            else:
+                print(f"Service {service} is not active on Ubuntu")
         elif os == "Alpine":
-            openrc.service(service, running=False, enabled=False)
-            print(f"Disabled service: {service} on Alpine")
+            service_status = host.run_shell_command(f"rc-service {service} status")
+            if "started" in service_status.stdout:
+                openrc.service(service, running=False, enabled=False)
+                print(f"Disabled service: {service} on Alpine")
+            else:
+                print(f"Service {service} is not active on Alpine")
         else:
             print(f"Unsupported OS: {os}, skipping service: {service}")
