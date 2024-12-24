0da8d7f5670217125352e19c690b89d96cf75ea0
mohamad abukhader <mohammad.abukhader@hotmail.com>
2024-12-24 14:59:29 +0400

    new file:   infraninja/security/common/ntp_hardening.py 	new file:   infraninja/security/templates/ntp.conf.j2

---
+++ infraninja/security/common/ntp_hardening.py
@@ -0,0 +1,22 @@
+from pyinfra.api import deploy
+from pyinfra.operations import files, server
+
+
+@deploy("NTP Hardening")
+def ntp_hardening():
+    files.template(
+        name='Upload NTP configuration',
+        src="../infraninja/security/templates/ntp.conf.j2",
+        dest="/etc/ntp.conf",
+        user='root',
+        group='root',
+        mode='644',
+    )
+
+    # Restart the NTP service to apply changes
+    server.service(
+        name='Restart NTP service',
+        service='ntp',
+        running=True,
+        restarted=True,
+    )
