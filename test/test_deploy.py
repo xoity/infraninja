a5cdfbae556b05d11049b66bf28e88cb4d69a1e9
mohamad abukhader <mohammad.abukhader@hotmail.com>
2024-12-24 22:38:55 +0400

    modified:   test/test_deploy.py

--- test/test_deploy.py
+++ test/test_deploy.py
@@ -1,12 +1,8 @@
 import pyinfra
 from pyinfra.api import deploy
-<<<<<<< HEAD
 from infraninja.security.common.ssh_hardening import ssh_hardening as task1
 
 
-=======
-from infraninja.security.common.ssh_hardening import ssh_hardening as task
->>>>>>> parent of 87df1a6 (	new file:   infraninja/security/alpine/dns_poisoning_protection_alpine.py)
 
 @deploy('Test Security Setup')
 def test_deploy():
