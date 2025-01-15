66e08f135c6600aca3a18a4d8c5c1a79a3afcedc
mohamad abukhader <mohammad.abukhader@hotmail.com>
2025-01-15 11:42:24 +0400

    modified:   test/test_deploy.py

--- test/test_deploy.py
+++ test/test_deploy.py
@@ -1,6 +1,6 @@
 import pyinfra
 from pyinfra.api import deploy
-from infraninja.security.alpine.install_tools import install_security_tools as task1
+from infraninja.security.common.kernel_hardening import kernel_hardening as task1
 from infraninja.security.common.ssh_hardening import ssh_hardening as task2
 
 
